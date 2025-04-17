c521d5a2c30a9e8fae8f4ff21752a7a4d1c8ebdb
Saku Tarkiainen <saku.tarkiainen@gmail.com>
2025-04-17 10:07:45 +0300

    Changed package.json and return result

--- src/backend/handler/app.js
+++ src/backend/handler/app.js
@@ -66,7 +66,7 @@ async function requestData(data, res) {
         res.status(200).send({
             "type": "response_result",
             "data": {
-                "result": true,
+                "result": "success",
                 "data": dbData
             }
         });
@@ -105,7 +105,7 @@ async function sendReservation(data, res) {
             res.status(200).send({
                 "type": "response_result",
                 "data": {
-                    "result": true,
+                    "result": "success",
                     "message": "Reservation made successfully."
                 }
             });
@@ -113,7 +113,7 @@ async function sendReservation(data, res) {
             res.status(500).send({
                 "type": "response_result",
                 "data": {
-                    "result": false,
+                    "result": "failure",
                     "message": "Could not make reservation."
                 }
             });
