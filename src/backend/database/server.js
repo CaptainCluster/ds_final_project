3fb78a5ad5f538c4b0ec6de0539ceebd1dda6f01
Leevi Lautanen <leevi.lautanen02@gmail.com>
2025-04-18 19:16:42 +0300

    Removed unused env variable

--- src/backend/database/server.js
+++ src/backend/database/server.js
@@ -5,7 +5,7 @@ import Reservation from "./models/Reservation.js";
 import { mockContractors } from "./mockData.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 
