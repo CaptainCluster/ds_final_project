1e096523bc33bd080ab14146017e8630eb032c8c
Leevi Lautanen <leevi.lautanen02@gmail.com>
2025-04-24 15:54:01 +0300

    Moved mock reservation creation into utils and fixed the reservation time bug

--- src/backend/database/server.js
+++ src/backend/database/server.js
@@ -17,55 +17,9 @@ try {
   // Delete existing contractors, should be removed in final version
   await Contractor.deleteMany({});
 
-  const mockData = mockContractors.map((contractor) => {
-    // Times in UTC + 3 (Finland)
-    const reservations = [];
-    const workDayStart = 11; // 8:00
-    const workDayStop = 19; // 16:00
-    const maxDays = 5;
-    const currentDate = new Date();
-    currentDate.setHours(currentDate.getHours() + 4);
-    currentDate.setMinutes(0, 0, 0);
-
-    // Create 5 day arrays
-    for (let day = 0; day < maxDays; day++) {
-      const daySlots = [];
-
-      // Skip weekends
-      const dayOfWeek = currentDate.getDay();
-      if (dayOfWeek === 0 || dayOfWeek === 6) {
-        currentDate.setDate(currentDate.getDate() + 1);
-        day--;
-        continue;
-      }
-
-      // Create 8 hour slots
-      for (let hour = 0; hour < 8; hour++) {
-        if (currentDate.getHours() + 1 > workDayStop) {
-          break;
-        }
-
-        daySlots.push({
-          reserved: false,
-          startDate: new Date(currentDate),
-        });
-
-        currentDate.setHours(currentDate.getHours() + 1);
-      }
-
-      // Add complete day to reservations
-      reservations.push(daySlots);
-
-      // Move to next day
-      currentDate.setDate(currentDate.getDate() + 1);
-      currentDate.setHours(workDayStart, 0, 0, 0);
-    }
-
-    return {
-      ...contractor,
-      reservations,
-    };
-  });
+  const mockData = mockContractors.map((contractor) =>
+    createMockReservations(contractor)
+  );
 
   await Contractor.insertMany(mockData);
 } catch (error) {
@@ -119,12 +73,14 @@ app.post("/reserve", cors(), async (req, res) => {
       return res.status(400).json({ error: "Invalid reservation time." });
     }
 
-    await Contractor.updateOne(
+    Contractor.updateOne(
       { _id: contractor._id },
+      { $set: { "reservations.$[outer].$[inner].reserved": true } },
       {
-        $set: {
-          [`reservations.${dayIndex}.${hourIndex}.reserved`]: true,
-        },
+        arrayFilters: [
+          { outer: { $type: "array" } },
+          { "inner.startDate": startDate },
+        ],
       }
     );
 
@@ -142,7 +98,7 @@ app.post("/reserve", cors(), async (req, res) => {
     console.error("Server error making a reservation:", error);
     res.status(500).json({
       error: "Error handling request.",
-      success: false
+      success: false,
     });
   }
 });
