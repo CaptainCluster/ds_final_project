4a7c00a963d994f9f6203d5d65c79cc0f9775f9d
CaptainCluster <sranta.ville@gmail.com>
2025-04-27 16:36:49 +0300

    /all route for handler

--- src/backend/handler/app.js
+++ src/backend/handler/app.js
@@ -34,6 +34,36 @@ app.get("/test", (req, res) => {
   });
 });
 
+app.get("/all", async (req, res) => {
+  // Pick a random database and request the data
+  const randomDbUrl = dbURLs[Math.floor(Math.random() * dbURLs.length)];
+
+  try {
+    const response = await fetch(randomDbUrl + "/all", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status !== 200) {
+      return res.status(response.status).json({
+          msg: "Error when attempting to fetch data."
+      });
+    }
+
+    // Wait for response data and parse it as json
+    const dbData = await response.json();
+
+    // Send the received data back to the client
+    res.status(200).json(dbData);
+  } catch (error) {
+    console.log("Error contacting DB server:", error);
+    sendErrorResponse(res, "Failed to fetch data from the database.");
+    return;
+  }
+});
+
 /**
  * A route for receiving contractor data
  *
