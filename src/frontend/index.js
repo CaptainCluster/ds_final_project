1ee0d43c4a4dac80d2b7b1602e4c467515f3ac90
CaptainCluster <sranta.ville@gmail.com>
2025-04-27 16:53:31 +0300

    Client API for data displayed

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -13,9 +13,31 @@ const fetchConsultantByName = async () => {
         "content-type": "application/json",
     },
   });
-  const data = await response.json();
-  console.log(data)
-  return data;
+  const consultantData = await response.json();
+  const container = document.getElementById("container");
+
+  // Creating an entry for each consultant and appending them to a container
+  consultantData.contractors.forEach((consultant) => {
+    const entry = document.createElement("div");
+    entry.className = "consultant-entry"
+
+    const nameP = document.createElement("p");
+    const emailP = document.createElement("p");
+
+    nameP.className = "consultant-name";
+    emailP.className = "consultant-email";
+  
+    nameP.textContent = consultant.name;
+    emailP.textContent = consultant.email;
+
+    entry.appendChild(nameP);
+    entry.appendChild(emailP)
+
+    container.appendChild(
+      entry
+    );
+  });
 }
 
-fetchConsultantByName();
+fetchConsultantByName()
+
