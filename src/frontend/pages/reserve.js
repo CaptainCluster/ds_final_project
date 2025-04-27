b04161c209b0abee18d7c998e702ddc6b7eae4f5
CaptainCluster <sranta.ville@gmail.com>
2025-04-27 18:56:12 +0300

    Client receives timeslots

--- src/frontend/pages/reserve.js
+++ src/frontend/pages/reserve.js
@@ -1,8 +1,28 @@
+const SERVER_PORT = 8000;
+
 const getEmailFromUrl = () => {
     let url = window.location.search.substring(1);
-    const email = url.split("=")[1];
-    return email;
+    const consultEmail = url.split("=")[1];
+    return consultEmail;
 }
 
-const email = getEmailFromUrl();
-console.log(email)
+const fetchTimeSlots = async (consultEmail) => {
+    const response = await fetch(`http://localhost:${SERVER_PORT}/fetch_data`, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify({
+            data: {
+                contractorEmail: consultEmail,
+            }
+        })
+    });
+    const data = await response.json();
+    const consultData = data.data.data;
+    console.log(consultData)    
+}
+
+const consultEmail = getEmailFromUrl();
+fetchTimeSlots(consultEmail)
+
