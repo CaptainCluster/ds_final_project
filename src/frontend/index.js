d37adc62df624807b58e6617b342399d1e115b0d
CaptainCluster <sranta.ville@gmail.com>
2025-04-27 16:39:28 +0300

    API for consultant data on client

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,4 +1,4 @@
-console.log("Testi!")
+const HANDLER_PORT = 8000;
 
 /**
  * @TODO Implement this, if necessary
@@ -7,14 +7,15 @@ console.log("Testi!")
  * to a node within the system.
  */ 
 const fetchConsultantByName = async () => {
-  const response = await fetch("http://localhost:5173/", {
+  const response = await fetch(`http://localhost:${HANDLER_PORT}/all`, {
     method: "GET",
     headers: {
         "content-type": "application/json",
     },
   });
-  const data = await response.json()
-  console.log(data);  
+  const data = await response.json();
+  console.log(data)
+  return data;
 }
 
 fetchConsultantByName();
